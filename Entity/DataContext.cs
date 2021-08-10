07072a87eeb3181f9f96c0b7a9b231f35fa3a447
= <=>
2021-08-11 02:19:46 +0300

    Entity Framework and Entity classes created!

---
+++ Entity/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BasicECommerce.Entity
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("dataConnection")
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
