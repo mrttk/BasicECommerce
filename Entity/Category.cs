d8b237b7ad56291f16ce2932f2691d2af27bffd7
= <=>
2021-08-12 20:58:19 +0300

    Product Controller, Category Controller and realational Views edited with scaffolding.

--- Entity/Category.cs
+++ Entity/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace BasicECommerce.Entity
     public class Category
     {
         public int Id { get; set; }
+        [Display(Name = "Category Name")]
         public string Name { get; set; }
         public string Description { get; set; }
 
--- Entity/Product.cs
+++ Entity/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace BasicECommerce.Entity
     public class Product
     {
         public int Id { get; set; }
+        [Display(Name = "Product Name")]
         public string Name { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
