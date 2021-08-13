7c61b73f2ee1a9bf7684bd7c2cd3e6f11231672d
= <=>
2021-08-14 00:12:26 +0300

    Cart Operations Added!

---
+++ Controllers/CartController.cs
@@ -0,0 +1,55 @@
+﻿using BasicECommerce.Entity;
+using BasicECommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicECommerce.Controllers
+{
+    public class CartController : Controller
+    {
+        private DataContext _db = new DataContext();
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View(GetCart());
+        }
+
+        public ActionResult AddToCart(int id)
+        {
+            var product = _db.Products.FirstOrDefault(i => i.Id == id);
+            if (product != null)
+            {
+                GetCart().AddProduct(product, 1);
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult RemoveFromCart(int id)
+        {
+            var product = _db.Products.FirstOrDefault(i => i.Id == id);
+            if (product != null)
+            {
+                GetCart().DeleteProduct(product);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public Cart GetCart()
+        {
+            var cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+
+        public PartialViewResult Summary()
+        {
+            return PartialView(GetCart());
+        }
+    }
+}
